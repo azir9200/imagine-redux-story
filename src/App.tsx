ce2d311993b480fec0b83b9be1856c2b3ccc98d1
Azir@9200 <aziruddin83@hotmail.com>
2024-11-14 16:45:57 +0000

    created initial projects

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import MainLayout from "./components/layouts/MainLayout";
+
+function App() {
+  return (
+    <div className=" min-h-screen w-full ">
+      <MainLayout />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CartDetails.tsx
@@ -0,0 +1,9 @@
+const CartDetails = () => {
+  return (
+    <div>
+      <h2>CartDetails</h2>
+    </div>
+  );
+};
+
+export default CartDetails;
