3ece0eed21eeb772659ca13800d2e9088599342b
Azir@9200 <aziruddin83@hotmail.com>
2025-05-04 21:00:05 +0100

    added

--- src/pages/Dashboard/WelcomeBanner.tsx
+++ src/pages/Dashboard/WelcomeBanner.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+ import user_img from "@/assets/images/user1.png"; 
+import { useGetMeQuery } from "../../redux/api/getMeApi/getMeApi";
+import { getToday } from "../../lib/getToday";
 
 const WelcomeBanner = () => {
+  const { data } = useGetMeQuery(undefined);
+   
+  const userData = data?.data;
+
+  const today = getToday();
+
   return (
-    <div>
-      <h1> Welcome to dashboard</h1>
+    <div className="flex justify-between bg-gradient-to-r from-primary to-purple-400 rounded-2xl text-white">
+      <div className="flex flex-col gap-4 justify-between p-8 md:p-10 xl:p-14">
+        <p className="text-sm md:text-base">{today}</p>
+        <div className="space-y-1">
+          <h2 className="text-2xl md:text-3xl xl:text-4xl font-bold">
+            Welcome back, {userData?.name || ""}!
+          </h2>
+          <p className="text-sm md:text-base">
+            Keep your profile updated to access the latest features.
+          </p>
+        </div>
+      </div>
+      <div className="hidden md:flex md:pr-10 xl:pr-14 pt-2 items-center">
+        <img src={user_img} alt="player image" className="w-full md:w-64" />
+      </div>
     </div>
   );
 };
